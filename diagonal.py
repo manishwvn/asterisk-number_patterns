e84fc118f1025a82add4b9327d2dde143d71fd49
manishwvn <48592499+manishwvn@users.noreply.github.com>
2020-08-19 15:37:06 +0530

    commit

---
+++ diagonal.py
@@ -0,0 +1,20 @@
+# to print the following pattern
+'''
+*
+    *
+        *
+            *
+                *
+
+'''
+n = 5
+
+for i in range(1, n + 1):
+  for j in range(1, n + 1):
+
+    if (i == j):
+      print('*\t', end = ' ')
+    else:
+      print('\t', end =' ')
+
+  print()
